b5046751571a15c6382e5efe13318eaadfa7278d
Taylor J <72883059+TaylorDJones11@users.noreply.github.com>
2024-09-05 14:35:42 -0500

    initial files

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+// entry point to Nextjs application
+import '@/assets/styles/globals.css';
+
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
