bd12472a66ad8a0ecb411e187c502063aa64d1f6
Taylor J <72883059+TaylorDJones11@users.noreply.github.com>
2024-09-06 14:47:56 -0500

    added assets

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,12 @@
 // entry point to Nextjs application
 import '@/assets/styles/globals.css';
 
+export const metadata = {
+  title: 'Globe Stay',
+  keywords: 'rental, property, real estate',
+  description: 'Find the perfect rental property',
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html>
