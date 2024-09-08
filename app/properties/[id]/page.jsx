02c1ede988bccb7af1276a3e8349461a94af6fef
Taylor J <72883059+TaylorDJones11@users.noreply.github.com>
2024-09-08 14:04:05 -0500

    property details component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -3,6 +3,7 @@ import connectDB from '@/config/database';
 import Property from '@/models/Property';
 import Link from 'next/link';
 import { FaArrowLeft } from 'react-icons/fa';
+import PropertyDetails from '@/components/PropertyDetails';
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
@@ -25,7 +26,7 @@ const PropertyPage = async ({ params }) => {
       <section class='bg-blue-50'>
         <div class='container m-auto py-10 px-6'>
           <div class='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
-            {/* {Property Info} */}
+            <PropertyDetails property={property} />
           </div>
         </div>
       </section>
